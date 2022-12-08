211fd54b3b1de90b1d679c0add81067d668fb63f
Jean Savarit <jean.savarit@yahoo.com>
2022-12-08 10:36:10 +0100

    2022 day 8 part 2

--- py/2022/day08.py
+++ py/2022/day08.py
@@ -11,7 +11,7 @@ def p1(f):
     forrest = readForrest(f)
     forrestDepth = len(forrest)
     forrestWidth = len(forrest[0])
-    result = forrestDepth * 2 + (forrestWidth - 2) * 2
+    result = forrestDepth * 2 + (forrestWidth - 2) * 2 #all edges are visible
     for i in range(1, forrestDepth-1):
         for j in range(1, forrestWidth-1):
             isVisible = False
@@ -30,4 +30,23 @@ def p1(f):
     return result
 
 def p2(f):
-    return 0
+    forrest = readForrest(f)
+    result = 0
+    forrestDepth = len(forrest)
+    forrestWidth = len(forrest[0])
+    for i in range(1, forrestDepth-1):
+        for j in range(1, forrestWidth-1):
+            for x in range(j+1, forrestWidth):
+                rightScore = abs(x-j)
+                if forrest[i][x] >= forrest[i][j]: break
+            for x in range(j-1, -1, -1):
+                leftScore = abs(x-j)
+                if forrest[i][x] >= forrest[i][j]: break
+            for x in range(i-1, -1, -1):
+                bottomScore = abs(x-i)
+                if forrest[x][j] >= forrest[i][j]: break
+            for x in range(i+1, forrestDepth):
+                topScore = abs(x-i)
+                if forrest[x][j] >= forrest[i][j]: break
+            result = max(result, leftScore * rightScore * topScore * bottomScore)
+    return result
