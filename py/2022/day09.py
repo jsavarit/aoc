8ad2c146a2141c447f75650d360277fd73ada67b
Jean <jean@P47.local>
2022-12-10 00:19:50 +0100

    2022 day 9 improved

--- py/2022/day09.py
+++ py/2022/day09.py
@@ -1,12 +1,9 @@
 def moovDatSnaik(f,snaikkSz):
-    tailMap = [[0, 0]]
-    x = [0] * snaikkSz
-    y = [0] * snaikkSz
+    tailMoves = set()
+    x, y = [0] * snaikkSz, [0] * snaikkSz
     for line in f:
-        move = line.split()
-        hdg = move[0]
-        dist = int(move[1])
-        for a in range(dist):
+        hdg, dist = line.split()
+        for _ in range(int(dist)):
             if hdg == 'R': x[0] += 1
             elif hdg == 'L': x[0] -= 1
             elif hdg == 'D': y[0] -= 1
@@ -16,13 +13,10 @@ def moovDatSnaik(f,snaikkSz):
                 yD = y[i] - y[i+1]
                 if(abs(xD) == 2 and abs(yD) == 1): y[i+1] = y[i]
                 if(abs(yD) == 2 and abs(xD) == 1): x[i+1] = x[i]
-                if(abs(xD) == 2): x[i+1] += xD//2
-                if(abs(yD) == 2): y[i+1] += yD//2
-            tailMap.append([x[-1], y[-1]])
-    uniqueMap = []
-    for x in tailMap:
-        if x not in uniqueMap: uniqueMap.append(x)
-    return len(uniqueMap)
+                x[i+1] += int(xD/2)
+                y[i+1] += int(yD/2)
+            tailMoves.add((x[-1], y[-1]))
+    return len(tailMoves)
 
 def p1(f):
     return moovDatSnaik(f, 2)
