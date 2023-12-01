7aa19e3516d16dc3ee254f5abe07c0b2255a4286
Jean Savarit <jean.savarit@yahoo.com>
2023-12-01 18:41:28 +0100

    day 1 improved

--- 2023/day01.py
+++ 2023/day01.py
@@ -1,7 +1,7 @@
 f = open("input/day01.txt").readlines()
 p1 = 0
 for line in f:
-    for char in line.strip():
+    for char in line:
         if char.isdigit(): 
             p1 += 10*int(char);
             break
@@ -12,26 +12,18 @@ for line in f:
         
 print(p1)
 
-DIGITS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+DIGITS = "one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9".split()
+INF = 1e9
 
 p2 = 0
 for line in f:
-    lpos, rpos, lval, rval = 1e9, 0, 0, 0
+    lpos, rpos, lval, rval = INF, -INF, 0, 0
     for i, digit in enumerate(DIGITS, 1):
-        l = line.find(digit)
-        r = line.rfind(digit)
+        i = (i+1) // 2
+        l, r = line.find(digit), line.rfind(digit)
         if (l >= 0 and l < lpos): lpos, lval = l, i
         if (r > rpos): rpos, rval = r, i
 
-    for i, char in enumerate(line):
-        if char.isdigit() and i<lpos:
-            lval = int(char)
-            break
-    for i, char in reversed(list(enumerate(line))):
-        if char.isdigit() and i>=rpos:
-            rval = int(char)
-            break
-    
     p2 += 10*lval + rval
 
 print(p2)
